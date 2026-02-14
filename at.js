2b760c4057950613be6d1a7a1999341c79fb869b
NearBloodweb <nearblood2007@gmail.com>
2026-02-14 21:30:40 +0530

    Add files via upload

---
+++ at.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+            apiKey: "AIzaSyAlK1bE2ay4UlOvfNqHTIWnWPKZzwHZrxc", // <--- YOUR API KEY
+            authDomain: "blooddonorfinder-db146.firebaseapp.com",
+            projectId: "blooddonorfinder-db146",
+            storageBucket: "blooddonorfinder-db146.firebasestorage.app",
+            messagingSenderId: "560945836174",
+            appId: "1:560945836174:web:5dabd309293acc8b3b19df"
+        };
